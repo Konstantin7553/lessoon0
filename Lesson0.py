4938a5c79ddc8a1a2905f5e67aee7fb13a1ea6bd
Konstantin7553 <krasik150@gmail.com>
2024-06-29 12:45:48 +0300

    Add files via upload

---
+++ Lesson0.py
@@ -0,0 +1,12 @@
+"1st program"
+print((9 ** 0.5) * 5)
+"2nd program"
+print(9.99 > 9.98, 1000 != 1000.1)
+"3rd program"
+print(1234 // 10)
+print(123 % 100)
+print(5678 // 10)
+print(567 % 100)
+print(23 + 67)
+"4th program"
+print(13.42 != 42.13) and print(13.42 == 42.13)
